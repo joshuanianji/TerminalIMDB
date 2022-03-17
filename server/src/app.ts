0433d905368c7658c6420370785010a063ab6210
Joshua Ji <joshua.ji@jiwhiz.com>
2022-03-17 12:09:04 -0600

    Docker stuff works!

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.send("Hello world!!");
 });
 
 export { app }
